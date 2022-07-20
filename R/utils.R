57a4bfc7dea3f87ed2cf08d81ace0a2bfa4d449d
saskiaotto <saskia.otto@uni-hamburg.de>
2022-07-20 23:05:01 +0200

    add new word template

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,7 @@
 # The following helper functions were adopted from the 'rticles' package
 #-----------------------------------------------------------------------
 
-find_resource <- function(template, file = 'template.tex') {
+find_resource <- function(template, file) {
   res <- system.file(
     "rmarkdown", "templates", template, "resources", file, package = "UHHformats"
   )
@@ -13,12 +13,20 @@ find_resource <- function(template, file = 'template.tex') {
 }
 
 
-# Helper function to create a custom format derived from pdf_document that
-# includes a custom LaTeX template
+# Helper function to create a custom format derived from  rmarkdown::pdf_document
+# that includes a custom LaTeX template
 pdf_document_format <- function(
-  format, template = find_resource(format, 'template.tex'), ...) {
+  format, template = find_resource(format, file = 'template.tex'), ...) {
   fmt <- rmarkdown::pdf_document(..., template = template)
   fmt$inherits <- "pdf_document"
-  fmt
+  return(fmt)
 }
 
+
+# Helper function to create a custom format derived from bookdown::word_document2
+# that includes a custom UHH Word template
+word_document_format <- function(
+  format, template = find_resource(format, file = 'uhh-template.docx'), ...) {
+  fmt <- bookdown::word_document2(..., reference_docx = template)
+  return(fmt)
+}
