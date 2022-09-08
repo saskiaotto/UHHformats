8fc3fa2a20a2b68df02e74b5f416d175f0885ede
saskiaotto <saskia.a.otto@gmail.com>
2022-09-08 17:23:53 +0200

    updated package help file

--- R/UHHformats.R
+++ R/UHHformats.R
@@ -1,22 +1,42 @@
-#' UHHformats: R Markdown templates designed for the University of Hamburg
+#' UHHformats: R Markdown/Quarto templates designed for the University of Hamburg
 #'
 #' A package for creating HTML, PDF and Microsoft Word documents, reports,
-#' and cheat sheets using R Markdown.
+#' and cheat sheets using either R Markdown or now also Quarto.
 #'
-#' @section html_simple:
-#' Creates a simple HTML document with a fixed table of content
-#' @section html_material:
-#' Creates a HTML document based on the bootstrap design 'material'
-#' @section word_doc:
-#' Creates a simple MS Word document in English (default) and German
-#' @section pdf_simple:
-#' Creates a simple PDF/LaTeX document in English (default) and German
-#' @section pdf_report:
-#' Creates a report-like PDF/LaTeX document in English (default) and German
-#' @section pdf_cheatsheet:
-#' Creates a simple cheatsheet with box layouts
-#' @section rmd_to_jupyter:
-#' Creates a Jupyter Notebook from a R Markdown file
+#' @section Quarto templates:
+#' The following templates can be created using the function \code{\link{create_quarto_doc}}.
+#' Simply set the \emph{template} argument to one of the following types:
+#' \itemize{
+#'   \item \emph{'html'}: Creates a simple HTML document with a
+#'     fixed navigation bar on the left side.
+#'   \item \emph{'word'}: Creates a simple MS Word document in English
+#'     (default) and German.
+#'   \item \emph{'pdf_simple'}: Creates a simple PDF/LaTeX document in
+#'     English (default) and German.
+#'   \item \emph{'pdf_report'}: Creates a report-like PDF/LaTeX document in
+#'     English (default) and German.
+#' }
+#'
+#'
+#' @section R Markdown templates:
+#' The following templates can be created using RStudio's IDE or by using the
+#' function \code{\link{create_rmd_doc}}:
+#' \itemize{
+#'   \item \code{\link{html_simple}}: Creates a simple HTML document with a
+#'     fixed table of content.
+#'   \item \code{\link{html_material}}: Creates a HTML document based on the
+#'     bootstrap design 'material'.
+#'   \item \code{\link{word_doc}}: Creates a simple MS Word document in English
+#'     (default) and German.
+#'   \item \code{\link{pdf_simple}}: Creates a simple PDF/LaTeX document in
+#'     English (default) and German.
+#'   \item \code{\link{pdf_report}}: Creates a report-like PDF/LaTeX document in
+#'     English (default) and German.
+#'   \item \code{\link{pdf_cheatsheet}}: Creates a simple PDF cheatsheet with
+#'     box layouts.
+#'   \item \code{\link{rmd_to_jupyter}}: Creates a Jupyter Notebook from a R
+#'     Markdown file.
+#' }
 #' @docType package
 #' @name UHHformats
 NULL
